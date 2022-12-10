c5a6bd968df9a71cde70046a539611b666f927f0
chavante01 <86327521+chavante01@users.noreply.github.com>
2022-12-10 18:50:21 -0400

    finalizando tela main

--- aplicativoDoRU/src/pages/home.js
+++ aplicativoDoRU/src/pages/home.js
@@ -6,7 +6,6 @@ import MaterialCommunityIcons  from 'react-native-vector-icons/MaterialCommunity
 import LinearGradient from 'react-native-linear-gradient';
 
 
-
 export default function Home(){ 
 
         return(
@@ -29,18 +28,33 @@ export default function Home(){
                 </LinearGradient>
                 <View style={styles.corpo}>
                     <View style={styles.areadosaldo}>
-                        
+                        <Text style={{fontSize:20, marginTop:'3%'}}>Seu saldo</Text>
+                        <View style={{flexDirection:'row',alignItems:'center', marginTop:'5%'}}>
+                            <Text style={{fontSize:15,marginRight:'1%'}}>R$</Text>
+                            <Text style={{fontSize:50}}>60</Text>
+                        </View>    
                     </View>
                     <View style={styles.areadoextratoeinformaçoes}>
                         <TouchableOpacity style={styles.botaoextrato}>
-
+                            <Text style={{marginTop:'4%',}}>Extratos</Text>
+                            <View style={{marginTop:'4.5%'}}>
+                                <Octicons name="checklist" size={34} color="green" />
+                            </View>
+                            
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.botaoinformaçoes}>
-                                
+                            <Text style={{marginTop:'4%',}} >Informações</Text>
+                            <View>
+                                <Ionicons name="ios-information-circle-outline" size={45} color="green"/>
+                            </View>
+                            
                         </TouchableOpacity>
-                        
                     </View>
                 </View>
+                
+                
+               
+                
             </View>
         );
 } 
@@ -84,17 +98,29 @@ const styles = StyleSheet.create({
         marginTop:'35%'
     },
     corpo:{
-        height:'60%',
+        height:'50%',
         width:'100%',
         flexDirection:'column',
         alignItems:'center',
         
+        
     },
     areadosaldo:{
+        flexDirection:'column',
+        alignItems:'center',
+        marginTop:'-10%',
         width:'85%',
         height:'40%',
         borderRadius:15,
-        backgroundColor:'#fef9ef',
+        backgroundColor:'#fffafb',
+        shadowColor: "#000",
+        shadowOffset: {
+	        width: 0,
+	        height: 9,
+        },
+        shadowOpacity: 0.48,
+        shadowRadius: 11.95,
+        elevation: 18,
     },
     areadoextratoeinformaçoes:{
         width:'100%',
@@ -104,19 +130,35 @@ const styles = StyleSheet.create({
         marginTop:'7%'
     },
     botaoextrato:{
+        alignItems:'center',
         width:'39.2%',
         height:'24%',
         borderRadius:15,
-        backgroundColor:'#fef9ef',
-        shadowColor:'black',
-        shadowOpacity:1,
-        shadowRadius:10
+        backgroundColor:'#fffafb',
+        shadowColor: "#000",
+        shadowOffset: {
+	        width: 0,
+	        height: 9,
+        },
+        shadowOpacity: 0.48,
+        shadowRadius: 11.95,
+        elevation: 18,
+
     },
     botaoinformaçoes:{
+        alignItems:'center',
         width:'39.2%',
         height:'24%',
         borderRadius:15,
-        backgroundColor:'#fef9ef',
+        backgroundColor:'#fffafb',
+        shadowColor: "#000",
+        shadowOffset: {
+	        width: 0,
+	        height: 9,
+        },
+        shadowOpacity: 0.48,
+        shadowRadius: 11.95,
+        elevation: 18,
     },
     
 
